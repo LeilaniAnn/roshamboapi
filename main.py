0f91277eb749623ca86de284d2daf5addacaccfa
Leilani Ann Raranga <retrosole@gmail.com>
2016-09-11 18:04:30 -0700

    feat: email reminder is now for moves instead of just coming back to the game

--- main.py
+++ main.py
@@ -11,27 +11,43 @@ from models import User
 
 
 class SendEmailReminderHandler(webapp2.RequestHandler):
-
     def get(self):
         """Send a reminder email to each User with an email about games.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
+        send_email = False
         for user in users:
-            subject = 'We Miss You!'
-            body = 'Hello {}, Come back to Roshambo!! Play now with Lizard and Spock added.'.format(
-                user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+            games = Game.query(
+                    ndb.AND(
+                    Game.game_over == False, 
+                    ndb.OR(Game.playerOne == user.key, 
+                           Game.playerTwo == user.key
+                          )
+                        )
+                    )
+            for game in games:
+                if game.playerOne == user.key:
+                    if len(game.player_one_weapons) < game.total_rounds:
+                        send_email = True
+                        break
+                else:
+                    if len(game.player_two_weapons) < game.total_rounds:
+                        send_email = True
+                        break
+            if send_email:
+                subject = 'We Miss You @ RoshamboAPI'
+                body = 'Hello {}, you have an unfinished round at RoshamboAPI, come back?'.format(user.name)
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
                            user.email,
                            subject,
                            body)
+
 # Sends a user an email when they create a User
 class SendUserEmail(webapp2.RequestHandler):
     def post(self):
         """Send an email upon User Creation"""
-        user = get_by_urlsafe(self.request.get('user_key'), User)
+        user = get_by_urlsafe(self.request.get('playerOne'), User)
         subject = 'Welcome!'
         body = "Welcome to Roshambo!"
         logging.debug(body)
@@ -42,16 +58,15 @@ class SendUserEmail(webapp2.RequestHandler):
                        body)
 
 
-class UpdateUserScoreHandler(webapp2.RequestHandler):
-
+class CacheUserStats(webapp2.RequestHandler):
     def post(self):
-        roshamboAPI._update_user_score(
-            self.request.get('user_name'), self.request.get('result'))
+        """Update user stats in memcache."""
+        roshamboAPI._cache_user_stats()
         self.response.set_status(204)
-
 
 app = webapp2.WSGIApplication([
     ('/crons/send_reminder', SendEmailReminderHandler),
-    ('/tasks/update_user_score', UpdateUserScoreHandler),
-    ('/tasks/send_welcome_email', SendUserEmail)
+    ('/tasks/send_welcome_email', SendUserEmail),
+    ('/tasks/cache_user_stats', CacheUserStats),
+
 ], debug=True)
